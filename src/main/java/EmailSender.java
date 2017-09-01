db4a86f6f94612ddfc89d76c16c9f0edc5f5b562
JavaWars <lazazerovich@mail.ru>
2017-09-01 11:06:31 +0300

    init

--- src/main/java/EmailSender.java
+++ src/main/java/EmailSender.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 public class EmailSender {
     public static void main(String[] args) {
 
-        EmailService emailService=new EmailService("pavlo.lazariev@nure.ua","r5pVJj0ur5pVJj0u");
+        EmailService emailService=new EmailService("email","password");
         for (int i=0;i<100;i++) {
             emailService.sendMessage("artem.krikun@nure.ua", "важное сообщение "+i, "ты лалка!");
         }
